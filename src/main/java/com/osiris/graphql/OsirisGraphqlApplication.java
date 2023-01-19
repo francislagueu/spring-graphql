e52b630323837cf81d7b67e0440d8f105a165118
Francis Lagueu <lagson6@gmail.com>
2023-01-18 17:27:48 -0800

    Initial commit

---
+++ src/main/java/com/osiris/graphql/OsirisGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.osiris.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsirisGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsirisGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osiris/graphql/OsirisGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiris.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsirisGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
